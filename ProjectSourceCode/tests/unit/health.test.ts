0414fed28d5eb1bac3969daea514e3434f677bb1
Payton Hsu <paytonhsu@outlook.com>
2025-11-11 01:48:01 -0700

    created test case for registration

--- ProjectSourceCode/tests/unit/health.test.ts
+++ ProjectSourceCode/tests/unit/health.test.ts
@@ -1,4 +1,6 @@
 import { describe, expect, it } from "vitest";
+import request from "supertest";
+import {app} from "../../src/server/app.ts";
 
 // Placeholder to keep CI green without importing Bun-specific modules.
 describe("placeholder", () => {
@@ -6,3 +8,15 @@ describe("placeholder", () => {
     expect(true).toBe(true);
   });
 });
+
+describe("Testing registration API", () =>{
+  it('positive: /register should register a new user and add their info to the database', async () =>{
+    const res = await request(app)
+      .post("/register")
+      .send({
+        //TODO: write data to send to db
+      })
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe("Success");
+  })
+})
