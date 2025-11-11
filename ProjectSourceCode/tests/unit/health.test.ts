2dd7b062c306b7d3d68a91855e7f5cff584dd3c2
Payton Hsu <paytonhsu@outlook.com>
2025-11-11 02:26:35 -0700

    moved api tests from health.test.ts to their own file

--- ProjectSourceCode/tests/unit/health.test.ts
+++ ProjectSourceCode/tests/unit/health.test.ts
@@ -1,6 +1,4 @@
 import { describe, expect, it } from "vitest";
-import request from "supertest";
-import {app} from "../../src/server/app.ts";
 
 // Placeholder to keep CI green without importing Bun-specific modules.
 describe("placeholder", () => {
@@ -9,14 +7,3 @@ describe("placeholder", () => {
   });
 });
 
-describe("Testing registration API", () =>{
-  it('positive: /register should register a new user and add their info to the database', async () =>{
-    const res = await request(app)
-      .post("/register")
-      .send({
-        //TODO: write data to send to db
-      })
-    expect(res.status).toBe(200);
-    expect(res.body.message).toBe("Success");
-  })
-})
