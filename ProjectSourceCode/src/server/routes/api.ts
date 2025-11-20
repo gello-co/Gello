91ef8f20a0249ffc15f349d31e048a104d5427dd
Payton Hsu <paytonhsu@outlook.com>
2025-11-20 01:05:02 -0700

    deleted redundant routes in api.ts

--- ProjectSourceCode/src/server/routes/api.ts
+++ ProjectSourceCode/src/server/routes/api.ts
@@ -31,30 +31,4 @@ router.use("/lists", listsRoutes);
 router.use("/tasks", tasksRoutes);
 router.use("/points", pointsRoutes);
 router.use("/sse", sseRoutes);
-
-// Note: Error handler is registered at app level in app.ts
-
-//CRUD user routes
-
-//"Create" user
-router.post("/register", (req, res) => {
-  const username = req.body.username;
-  const email = req.body.email;
-  try {
-    res.status(200).send("Success");
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-//CRUD board routes
-
-router.get("/viewBoards", async (req, res) => {});
-
-router.post("/createBoard", async (req, res) => {});
-
-router.put("/updateBoard", async (req, res) => {});
-
-router.delete("/deleteBoard", async (req, res) => {});
-
 export default router;
